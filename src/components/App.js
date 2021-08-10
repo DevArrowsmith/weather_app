ae529301ff284b788bd80ef34052edc5e364a88b
gummicode <gummicodeblog@gmail.com>
2021-08-10 14:48:56 +0100

    Create LocationDetails component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "../styles/App.css";
+import LocationDetails from "./LocationDetails";
 
-function App() {
+const App = ({ location }) => {
+  const { city, country } = location;
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <LocationDetails city={city} country={country} />
     </div>
   );
-}
+};
 
 export default App;
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};

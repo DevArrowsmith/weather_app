be745e800e79a1b0ffc1c86e19b00a2938354b8a
devarrowsmith <devarrowsmith@gmail.com>
2021-08-19 21:02:53 +0100

    Update app to fetch and render live weather data.

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,44 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../styles/App.css";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 
-const App = ({ forecasts, location }) => {
-    const { city, country } = location;
+const App = () => {
+    const [forecasts, setForecasts] = useState([]);
+    const [location, setLocation] = useState({ city: "", country: "" });
+    const [selectedDate, setSelectedDate] = useState(0);
 
-    const [selectedDate, setSelectedDate] = useState(forecasts[0].date);
-    const handleForecastSelect = (date) => {
-        setSelectedDate(date);
+    const getForecast = () => {
+        const endpoint = "https://mcr-codes-weather-app.herokuapp.com/forecast";
+
+        axios.get(endpoint).then((response) => {
+            setSelectedDate(response.data.forecasts[0].date);
+            setForecasts(response.data.forecasts);
+            setLocation(response.data.location);
+        });
     };
+
+    useEffect(() => {
+        getForecast();
+    }, []);
+
     const selectedForecast = forecasts.find(
         (forecast) => forecast.date === selectedDate
     );
 
+    const handleForecastSelect = (date) => {
+        setSelectedDate(date);
+    };
+
     return (
         <div className="weather-app">
             <h1>Weather App</h1>
-            <LocationDetails city={city} country={country} />
-            <ForecastDetails forecast={selectedForecast} />
+            <LocationDetails city={location.city} country={location.country} />
+            {selectedForecast && (
+                <ForecastDetails forecast={selectedForecast} />
+            )}
             <ForecastSummaries
                 forecasts={forecasts}
                 onForecastSelect={handleForecastSelect}
@@ -30,21 +48,3 @@ const App = ({ forecasts, location }) => {
 };
 
 export default App;
-
-App.propTypes = {
-    forecasts: PropTypes.arrayOf(
-        PropTypes.shape({
-            date: PropTypes.number,
-            description: PropTypes.string,
-            icon: PropTypes.string,
-            temperature: PropTypes.shape({
-                max: PropTypes.number,
-                min: PropTypes.number,
-            }),
-        })
-    ).isRequired,
-    location: PropTypes.shape({
-        city: PropTypes.string,
-        country: PropTypes.string,
-    }).isRequired,
-};

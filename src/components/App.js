63e20ec9b3b56f69562ca54f91e1ba1ad25b6dad
devarrowsmith <devarrowsmith@gmail.com>
2021-08-22 19:23:48 +0100

    Add city search functionality.

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,50 @@
 import React, { useState, useEffect } from "react";
 import "../styles/App.css";
+import SearchForm from "./SearchForm";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 import getForecast from "../requests/getForecast";
 
 const App = () => {
+    const [locationSearchText, setLocationSearchText] = useState("");
     const [forecasts, setForecasts] = useState([]);
     const [location, setLocation] = useState({ city: "", country: "" });
     const [selectedDate, setSelectedDate] = useState(0);
     useEffect(() => {
-        getForecast(setSelectedDate, setForecasts, setLocation);
+        getForecast(
+            locationSearchText,
+            setSelectedDate,
+            setForecasts,
+            setLocation
+        );
     }, []);
 
-    const selectedForecast = forecasts.find(
-        (forecast) => forecast.date === selectedDate
-    );
+    const handleCitySearch = () => {
+        getForecast(
+            locationSearchText,
+            setSelectedDate,
+            setForecasts,
+            setLocation
+        );
+    };
 
     const handleForecastSelect = (date) => {
         setSelectedDate(date);
     };
 
+    const selectedForecast = forecasts.find(
+        (forecast) => forecast.date === selectedDate
+    );
+
     return (
         <div className="weather-app">
             <h1>Weather App</h1>
+            <SearchForm
+                searchText={locationSearchText}
+                setSearchText={setLocationSearchText}
+                onSubmit={handleCitySearch}
+            />
             <LocationDetails city={location.city} country={location.country} />
             {selectedForecast && (
                 <ForecastDetails forecast={selectedForecast} />
---
+++ src/components/SearchForm.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "../styles/SearchForm.css";
+
+const SearchForm = ({ searchText, setSearchText, onSubmit }) => {
+    const handleInputChange = (event) => setSearchText(event.target.value);
+
+    return (
+        <div className="search-form">
+            <input
+                type="text"
+                placeholder="Location"
+                onChange={handleInputChange}
+                value={searchText}
+            />
+            <button type="submit" onClick={onSubmit}>
+                Search
+            </button>
+        </div>
+    );
+};
+
+SearchForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    searchText: PropTypes.string.isRequired,
+    setSearchText: PropTypes.func.isRequired,
+};
+
+export default SearchForm;

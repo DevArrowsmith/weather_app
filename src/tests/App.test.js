a8e9a17c92ef463d54441dcca4aba7129422989d
devarrowsmith <devarrowsmith@gmail.com>
2021-08-22 18:06:53 +0100

    Update App component tests: Add snapshot test.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,8 +3,17 @@ import { render, screen } from "@testing-library/react";
 import App from "../components/App";
 import { location, forecasts } from "../data/mock-forecast.json";
 
-test("renders Weather App title", () => {
-    render(<App forecasts={forecasts} location={location} />);
-    const titleElement = screen.getByText(/Weather App/i);
-    expect(titleElement).toBeInTheDocument();
+describe("App component", () => {
+    test("Renders correctly (snapshot)", () => {
+        const { asFragment } = render(
+            <App forecasts={forecasts} location={location} />
+        );
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    test("renders Weather App title", () => {
+        render(<App forecasts={forecasts} location={location} />);
+        const titleElement = screen.getByText(/Weather App/i);
+        expect(titleElement).toBeInTheDocument();
+    });
 });
